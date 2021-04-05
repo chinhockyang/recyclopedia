b520b4f25217f869f7b00bf764be5bc43c8edc3d
Choong Yong Xuan <67458324+yxchoong@users.noreply.github.com>
2021-04-06 01:03:03 +0800

    edit

--- src/components/User/Donation.vue
+++ src/components/User/Donation.vue
@@ -1,8 +1,8 @@
 <template> 
 <div> 
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9bNIk4c3ib8JXCfzqcOzqYxnAFzZQIGmjOa-WCNWJpzNisCg4aUzVD5KYTRmL4AeveU&usqp=CAU">
-    <h4><strong>Garden City Fund Donation</strong></h4>
-    <p><strong>Donate $2 for every 300 points</strong></p>
+    <h4 id="hd"><strong>Garden City Fund Donation</strong></h4>
+    <p id="pd"><strong>Donate $2 for every 300 points</strong></p>
     <div> 
         <form> 
         </form>
@@ -18,12 +18,12 @@ img {
   width: 20%;
 }
 
-h4 {
+#hd {
     text-align: center;
     color: darkgreen
 }
 
-p {
+#pd {
     text-align: center;
     font-size: 17px;
 }
