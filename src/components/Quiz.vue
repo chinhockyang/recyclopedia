1b8ca789516ff9087466322feab47f54b6972d3d
claratay <80338381+claratay@users.noreply.github.com>
2021-04-05 11:00:59 +0800

    Added quiz to NavMenu

---
+++ src/components/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <mdb-edge-header color="indigo"/>
+      
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
