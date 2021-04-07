ac52e5109590817f9c3427c367de10ebdaddb82c
darellelogram <darrell.lai@gmail.com>
2021-04-07 21:55:59 +0800

    Changed implementation of geospatial data. Instead of repeatedly querying the OneMap API for a certain area, I queried once for all bins in Singapore, downloaded the data and wrote a .py script to reformat it into GeoJson. Then the GeoJson data is loaded into the Google Maps API. This circumvents the problem where the OneMap API token had to be renewed every three days.

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -2,8 +2,6 @@
   <div>
     <h3>Find My Bin</h3>
     <div id="map"></div>
-    <!-- <div id="capture"></div> -->
-    <!-- <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4Qf39SqcEAphfabQgzdx7eaeGA-t8he0&callback=initMap"></script> -->
   </div>
 </template>
 
@@ -23,13 +21,13 @@ import axios from 'axios';
 export default {
   data () {
     return {
-      // map: ,
+      map: null,
       src: "http://drive.google.com/uc?id=1WrA6xSjaGgZel3Ii8JZR_cKzRrCmv4ag", // kml file source
-      // src: "http://drive.google.com/uc?id=1xG70dBfmZVsuLt27GyV9PmY-c1PF9OMk",
-      // kmlLayer
       points: [],
       OneMap_API_KEY: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjczMjcsInVzZXJfaWQiOjczMjcsImVtYWlsIjoiZGFycmVsbC5sYWlAZ21haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjE3NzAyNzU4LCJleHAiOjE2MTgxMzQ3NTgsIm5iZiI6MTYxNzcwMjc1OCwianRpIjoiNGU2ZjkzMmJhODQxNTRkM2UwMzJkZTFlZGEwNTcyNzUifQ.X-pQ8Z3Wkb6u-7BB7Ud7t2gEu4dzukZ9lkyEfAGpZ34',
-      OneMap_token_expiry: 9999999999
+      OneMap_token_expiry: 9999999999,
+      circles: [],
+      bins: []
     }
   },
   methods: {
@@ -50,8 +48,8 @@ export default {
     getKMradius: function(lat, lng) {
       // return coordinates of bounding box of 2km x 2km
         // just need 2 corners
-      return {topright: [lat+0.01, lng + 0.01], bottomleft: [lat-0.01, lng-0.01]};
-
+      return {topright: [lat+0.02, lng + 0.02], bottomleft: [lat-0.02, lng-0.02]};
+      // return {topright: [lat+0.1, lng + 0.1], bottomleft: [lat-0.1, lng-0.1]};
     },
     getExtentsQuery: function(bb) {
       let extents = bb.bottomleft[0] + ",%20" + bb.bottomleft[1] + "," + bb.topright[0] + ",%20" + bb.topright[1];
@@ -68,40 +66,97 @@ export default {
           if (bin.LatLng) {
             let latlng_arr = bin.LatLng.split(",");
             this.points.push({lat: parseFloat(latlng_arr[0]), lng: parseFloat(latlng_arr[1])});
-            console.log("pushing bin into this.points now!");
-            console.log("latlng_arr[0] = " + latlng_arr[0] + ", latlng_arr[1] = " + latlng_arr[1]);
-            console.log("this.points has length " + this.points.length + " now");
+            // console.log("pushing bin into this.points now!");
+            // console.log("latlng_arr[0] = " + latlng_arr[0] + ", latlng_arr[1] = " + latlng_arr[1]);
+            // console.log("this.points has length " + this.points.length + " now");
           }
         })
       })
-    } 
+    },
+    fetchAndRenderBins: async function(center) {
+      // Fetch the Bins from the data source
+      this.bins = (await this.fetchBins(center)).features;
+
+      // Create circular markers based on the stores
+      this.circles = this.bins.map((bin) => this.binToCircle(bin, this.map));
+    },
+    fetchBins: async function(center){
+      const url = `testbins.json`;
+      const response = await fetch(url);
+      console.log(center);
+      try { 
+        console.log(response.json()); 
+      } catch(error) {
+        console.log('Error happened here!');
+        console.error(error);
+      }
+      return response;
+    },
+    binToCircle: async function(bin, map, infowindow) {
+      const [lng, lat] = bin.geometry.coordinates;
+      const circle = new this.google.maps.Circle({
+        radius: 50,
+        strokeColor: "#579d42",
+        strokeOpacity: 0.8,
+        strokeWeight: 5,
+        center: { lat, lng },
+        map,
+      });
+      circle.addListener("click", () => {
+        infowindow.setContent(`${bin.properties.business_name}<br />
+          ${bin.properties.address_address}<br />
+          Austin, TX ${bin.properties.zip_code}`);
+        infowindow.setPosition({ lat, lng });
+        infowindow.setOptions({ pixelOffset: new this.google.maps.Size(0, -30) });
+        infowindow.open(map);
+      });
+      return circle;
+    }
   },
   async mounted() {
     try {
-      const google = await gmapsInit();
-      const geocoder = new google.maps.Geocoder();
+      this.google = await gmapsInit();
+      const geocoder = new this.google.maps.Geocoder();
       // const map = new google.maps.Map(this.$el);
-      const map = new google.maps.Map(document.getElementById("map"));
+      this.map = new this.google.maps.Map(document.getElementById("map"));
 
       geocoder.geocode({ address: 'Singapore' }, (results, status) => {
         if (status !== 'OK' || !results[0]) {
           throw new Error(status);
         }
-        map.setCenter(results[0].geometry.location);
-        map.fitBounds(results[0].geometry.viewport);
+        this.map.setCenter(results[0].geometry.location);
+        this.map.fitBounds(results[0].geometry.viewport);
       });
 
-      var layer_bins = new google.maps.KmlLayer(this.src, {
-        suppressInfoWindows: true,
-        preserveViewport: false,
-        map: map
-      });
+      // Load the GeoJson bins data onto the map 
+      try {
+        console.log('loading GeoJson here!');
+        // map.data.loadGeoJson('http://drive.google.com/uc?id=1_LivKGKN37UCxgIMlBqJRPzj4lBY8rip');
+        // map.data.loadGeoJson('http://127.0.0.1:8887/src/components/map/recyclebinsnew.json'); 
+        this.map.data.loadGeoJson('http://127.0.0.1:8887/src/components/map/testbins.json'); 
+        // this.map.data.loadGeoJson('./testbins.json'); 
+        // map.data.loadGeoJson(String.raw`C:\Users\darre\Documents\zzz Old files\NUS Y2S2\BT3103\project\bt3103-project\src\components\map\testbins.json`);
+      } catch(error) {
+        console.log('Error happened here!');
+        console.error(error);
+      }
 
-      console.log("layer_bins.KmlLayer: " + layer_bins.KmlLayer);
-      layer_bins.addListener('click', function(event) {
-        var content = event.featureData.infoWindowHtml;
-        var testimonial = document.getElementById('capture');
-        testimonial.innerHTML = content;
+      // this.fetchAndRenderBins({lat: 1.3521, lng: 103.8198});
+
+      const infoWindow = new this.google.maps.InfoWindow();
+      // console.log(map.data);
+      // Show information for each bin when its marker is clicked
+      this.map.data.addListener('click', (event) => {
+        console.log('event is ----- ' + event);
+        const address = event.feature.getProperty('address');
+        const postcode = event.feature.getProperty('postcode');
+        const position = event.feature.getGeometry().get();
+        const content = `<h4>${address}</h4><p>${postcode}</p>`;
+
+        infoWindow.setContent(content);
+        infoWindow.setPosition(position);
+        infoWindow.setOptions({pixelOffset: new this.google.maps.Size(0, -30)});
+        infoWindow.open(this.map);
       });
       
       // implement geolocation
@@ -112,57 +167,57 @@ export default {
         // they can navigate there using the full features of the app
       // TODO: implement support for phone web view - plist (permissions list)
       // TODO: doesn't work for network server
-      let infoWindow = new google.maps.InfoWindow();
-      const locationButton = document.createElement("button");
-      locationButton.textContent = "Show bins near me";
-      locationButton.classList.add("custom-map-control-button");
-      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-      locationButton.addEventListener("click", async () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              };
-              infoWindow.setPosition(pos);
-              infoWindow.setContent("Location found.");
-              infoWindow.open(map);
-              map.setCenter(pos);
-              map.setZoom(14);
-              console.log("pos.lat = " + pos.lat + ", pos.lng = " + pos.lng);
+      // let infoWindow = new google.maps.InfoWindow();
+      // const locationButton = document.createElement("button");
+      // locationButton.textContent = "Show bins near me";
+      // locationButton.classList.add("custom-map-control-button");
+      // map.controls[this.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+      // locationButton.addEventListener("click", async () => {
+      //   // Try HTML5 geolocation.
+      //   if (navigator.geolocation) {
+      //     navigator.geolocation.getCurrentPosition(
+      //       (position) => {
+      //         const pos = {
+      //           lat: position.coords.latitude,
+      //           lng: position.coords.longitude,
+      //         };
+      //         // infoWindow.setPosition(pos);
+      //         // infoWindow.setContent("Location found.");
+      //         // infoWindow.open(map);
+      //         map.setCenter(pos);
+      //         map.setZoom(14);
+      //         console.log("pos.lat = " + pos.lat + ", pos.lng = " + pos.lng);
 
-              // call One Map API
-              Promise.resolve(this.getBinsFromOneMap(pos))
-                .then(() => {
-                  for (const point of this.points) {
-                    // TODO: style each marker to look like a recycle bin
-                    new google.maps.Marker({
-                      position: point,
-                      map: map
-                    });
-                    console.log("point.lat = " + point.lat + ", point.lng = " + point.lng);
-                  }
-                  console.log("this.points.length = " + this.points.length);
-                });
+      //         // call One Map API
+      //         Promise.resolve(this.getBinsFromOneMap(pos))
+      //           .then(() => {
+      //             for (const point of this.points) {
+      //               // TODO: style each marker to look like a recycle bin
+      //               new this.google.maps.Marker({
+      //                 position: point,
+      //                 map: map
+      //               });
+      //               // console.log("point.lat = " + point.lat + ", point.lng = " + point.lng);
+      //             }
+      //             // console.log("this.points.length = " + this.points.length);
+      //           });
 
-              // get all recycle bins within a certain radius
-              // iterate over all bins and display on map
-              // const randomBin = {lat: 1.30367485182574,lng: 103.782905325792};
-              // const randomBin2 = {lat: 1.30790649968423,lng: 103.782958922488};
-              // const randomBin3 = {lat: 1.30846777384426,lng: 103.78649944798};
-              // const randomBinArr = [randomBin, randomBin2, randomBin3];
-            },
-            () => {
-              this.handleLocationError(true, infoWindow, map.getCenter());
-            }
-          );
-        } else {
-          // Browser doesn't support Geolocation
-          this.handleLocationError(false, infoWindow, map.getCenter(), map);
-        }
-      });
+      //         // get all recycle bins within a certain radius
+      //         // iterate over all bins and display on map
+      //         // const randomBin = {lat: 1.30367485182574,lng: 103.782905325792};
+      //         // const randomBin2 = {lat: 1.30790649968423,lng: 103.782958922488};
+      //         // const randomBin3 = {lat: 1.30846777384426,lng: 103.78649944798};
+      //         // const randomBinArr = [randomBin, randomBin2, randomBin3];
+      //       },
+      //       () => {
+      //         this.handleLocationError(true, infoWindow, map.getCenter());
+      //       }
+      //     );
+      //   } else {
+      //     // Browser doesn't support Geolocation
+      //     this.handleLocationError(false, infoWindow, map.getCenter(), map);
+      //   }
+      // });
     } catch (error) {
       console.error(error);
     }
@@ -171,17 +226,21 @@ export default {
 </script>
 
 <style scoped>
-  html, body {
-    height: 370px;
+  div {
+    height: 500px;
+    width: 1000px;
+    /* height: 370px; */
     padding: 0;
-    margin: 0;
+    margin: auto;
   }
   #map {
-    height: 360px;
-    width: 600px;
+    height: 85%;
+    /* height: 470px; */
+    /* width: 1000px; */
     overflow: hidden;
     float: left;
     border: thin solid #333;
+    margin: auto
   }
   #capture {
     height: 360px;
