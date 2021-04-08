9aca088d8d60c6afa20673e12dbbec76de3507b5
darellelogram <darrell.lai@gmail.com>
2021-04-08 22:28:51 +0800

    Included data for all types of recycling/collection points, updated logic of python script that parses regular json to GeoJson.

--- src/components/map/data/parseJson.py
+++ src/components/map/data/parseJson.py
@@ -1,53 +1,50 @@
 import json
-with open('recyclebins.json') as f:
-  data = json.load(f)
-print(data['SrchResults'][0])
-print(data['SrchResults'][1])
 
-bins = data['SrchResults']
-print(len(bins))
+def jsonToGeoJson(infile, outfile, trunc=None):
+  with open(infile) as f:
+    data = json.load(f)
+  points = data['SrchResults']
+  
+  newpoints = []
+  # exclude first ele which contains metadata
+  for point in points[1:]:
+    newpoint = {}
+    latlng = point['LatLng'].split(',')
+    lat = float(latlng[0])
+    lng = float(latlng[1])
+    newpoint['geometry'] = {"type": "Point", "coordinates": [lng,lat]}
+    newpoint['type'] = "Feature"
 
-keys = bins[0].keys()
-print(keys)
-keys = bins[1].keys()
-print(keys)
+    # repopulate address details in GeoJson format
+    postcode = point['ADDRESSPOSTALCODE']
+    address = str(point['ADDRESSBLOCKHOUSENUMBER'])
+    if 'ADDRESSSTREETNAME' in point.keys():
+      address += ' ' + str(point['ADDRESSSTREETNAME'])
+    if 'ADDRESSBUILDINGNAME' in point.keys():
+      address = point['ADDRESSBUILDINGNAME'] + ', ' + address
+    if 'ADDRESSUNITNUMBER' in point.keys():
+      address += ', ' + point['ADDRESSUNITNUMBER']
+    
+    # add all needed properties to new Feature
+    # add a boolean variable to toggle the visibility of markers rendered on the map
+    newpoint['properties'] = {"address": address, "postcode": postcode, "showMarker": True}
 
-# GeoJson format
-# {
-#   "geometry": {
-#     "type": "Point",
-#     "coordinates": [lat, lng]
-#     },
-#   "type": "Feature",
-#   "properties": {
-#     # arbitrary properties
-#   }
-# }
+    newpoints.append(newpoint)
+  
+  # cut off how many features to parse
+  end = len(newpoints)
+  if trunc:
+    end = trunc
 
-newbins = []
-for bin in bins[1:]:
-  newbin = {}
-  latlng = bin['LatLng'].split(',')
-  lat = float(latlng[0])
-  lng = float(latlng[1])
-  newbin['geometry'] = {"type": "Point", "coordinates": [lng,lat]}
-  newbin['type'] = "Feature"
-  address = str(bin['ADDRESSBLOCKHOUSENUMBER']) + ' ' + str(bin['ADDRESSSTREETNAME'])
-  postcode = bin['ADDRESSPOSTALCODE']
-  newbin['properties'] = {"address": address, "postcode": postcode}
-  newbins.append(newbin)
-
-geoJson = {
-  "type": "FeatureCollection",
-  "features": newbins
-}
-
-with open('recyclebinsGeo.json', 'w') as newfile:
-  json.dump(geoJson, newfile)
-
-geoJson_trunc = {
-  "type": "FeatureCollection",
-  "features": newbins[:3]
-}
-with open('testbins.json', 'w') as newtestfile:
-  json.dump(geoJson_trunc, newtestfile)
+  geoJson = {
+    "type": "FeatureCollection",
+    "features": newpoints[:end]
+  }
+  with open(outfile, 'w') as f:
+    json.dump(geoJson, f)
+jsonToGeoJson('./recyclebins.json', './testbinsGeo.json', 3)
+jsonToGeoJson('./recyclebins.json', './recyclebinsGeo.json')
+jsonToGeoJson('./ewaste.json', './ewasteGeo.json')
+jsonToGeoJson('./secondhandcollecn.json', './secondhandcollecnGeo.json')
+jsonToGeoJson('./rvm.json', './rvmGeo.json')
+jsonToGeoJson('./lighting.json', 'lightingGeo.json')
