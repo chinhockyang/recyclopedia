dd813d482f4fac2c1c5f083ed71b2f158f968018
Choong Yong Xuan <67458324+yxchoong@users.noreply.github.com>
2021-04-09 15:37:24 +0800

    'edits'

--- src/components/User/addNewRecord.vue
+++ src/components/User/addNewRecord.vue
@@ -2,68 +2,70 @@
   <div> 
     <div class="containter px-3"> 
       <br>
+      <div>
       <h4>Instructions</h4> 
       <ol> 
         <li>Identify the "AXXXXXX" Bin Serial Number which is located on the front face of the bin.</li> 
+        <img src="https://i.ibb.co/h9qB015/photo-2021-04-05-00-14-20.jpg" style="height: 200px">
         <li>Choose the item category from the dropdown list provided.</li>
         <li>Count the number of distinct items recycled for the specific item category.</li> 
       </ol>
       <h6>Recycling is a contribution to the environment.  We greatly appreciate if you can input your record honestly!</h6>
-
-    </div>
+      </div>
     <br>
-    <h2 id="center">Add New Record</h2>
-    <div class="row p-3 mb-5">
-    <div class="col-1 col-md-2 col-l-3"></div>
-    <div class="col-10 col-md-8 col-l-6 bg-light rounded p-3">
-      <div id="center">
-    <form class="containter px-3"> 
-        <div class="form-group">
-          <label>Bin Serial Number:<span style="color:red;" title="required"> *</span></label>
-          <br>
-          <div class="input-box">
-            <input type="number" class="form-control" id="serial" placeholder="XXXXXX" 
-             oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-             name="serialNo" pattern="\d*" maxlength="6" v-model="form.serialNo" required>
-             <span class="unit"><strong>A</strong></span>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="itemCat">Item Category<span style="color:red;" title="required"> *</span></label>
-            <br><select id="itemCat" name="itemCat" v-model="form.itemCat" class="form-control" required>
-                <option value="Plastic">Plastic</option>
-                <option value="Paper">Paper</option>
-                <option value="Metal">Metal</option>
-                <option value="Glass">Glass</option>
-                <option value="E-waste">E-waste</option>
-                <option value="Electrical">Electrical</option>
-            </select>
-        </div>
-        <div class="form-group">
-          <label for="quantity">Quantity<span style="color:red;" title="required"> *</span></label>
-            <br><input type="number" class="form-control" placeholder="0" id="quantity" name="quantity" v-model="form.quantity" required>
-            <div>
-            <p>5 points will be rewarded for every quantity recycled.</p>
+    <div>
+      <h2 id="center">Add New Record</h2>
+      <div class="row p-3 mb-5">
+        <div class="col-1 col-md-2 col-l-3"></div>
+          <div class="col-10 col-md-8 col-l-6 bg-light rounded p-3">
+            <div id="center">
+              <form class="containter px-3"> 
+                <div class="form-group">
+                  <label>Bin Serial Number:<span style="color:red;" title="required"> *</span></label>
+                  <br>
+                  <div class="input-box">
+                    <input type="number" class="form-control" id="serial" placeholder="XXXXXX" 
+                      oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                      name="serialNo" pattern="\d*" maxlength="6" v-model="form.serialNo" required>
+                    <span class="unit"><strong>A</strong></span>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="itemCat">Item Category<span style="color:red;" title="required"> *</span></label>
+                    <br><select id="itemCat" name="itemCat" v-model="form.itemCat" class="form-control" required>
+                        <option value="Plastic">Plastic</option>
+                        <option value="Paper">Paper</option>
+                        <option value="Metal">Metal</option>
+                        <option value="Glass">Glass</option>
+                        <option value="E-waste">E-waste</option>
+                        <option value="Electrical">Electrical</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                  <label for="quantity">Quantity<span style="color:red;" title="required"> *</span></label>
+                    <br><input type="number" class="form-control" placeholder="0" id="quantity" name="quantity" min="0" v-model="form.quantity" required>
+                    <div>
+                      <p>5 points will be rewarded for every quantity recycled.</p>
+                    </div>
+                </div>
+                <br>
+                <div id="center">
+                  <button @click.prevent="submitRecord" class="btn btn-success" >Submit</button>
+                </div>
+                <div>
+                  <div id="center">
+                    <p id="error" v-if="error">{{error}}</p>
+                  </div>
+                  <div id="center">
+                    <p style="font-size: 15px">Thank you for recycling today!</p>
+                  </div>
+                </div>
+              </form>
             </div>
-        </div>
-        <br>
-        <div id="center">
-          <button @click.prevent="submitRecord" class="btn btn-success" >Submit</button>
-        </div>
-        <div>
-          <div id="center">
-          <p id="error" v-if="error">{{error}}</p>
-          </div>
-          <div id="center">
-          <p style="font-size: 15px">Thank you for recycling today!</p>
           </div>
         </div>
-    </form>
-    </div>
-    </div>
-    </div>
-    
-    
+      </div>
+    </div>  
   </div>
 </template>
 
@@ -156,6 +158,10 @@ export default {
 #serial { 
   display: block; 
   padding: 7px 0px 7px 20px; 
+}
+
+.right {
+  float: right;
 }
 
 </style>
