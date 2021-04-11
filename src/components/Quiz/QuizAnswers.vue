94abba09039b5ad7c35d5dfd150b47bd4fab1d8a
claratay <80338381+claratay@users.noreply.github.com>
2021-04-11 23:35:35 +0800

    cleaning code

--- src/components/Quiz/QuizAnswers.vue
+++ src/components/Quiz/QuizAnswers.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
-
-  <div id="quizQuestions">
     <div v-for="(question, index) in quiz.questions" :key="index">
-
       <div class="row align-items-center justify-content-center text-center mt-3">
-        <div id = "questionContainer" class="col-4" style="background-color: #E3E9BB">
+        <div id = "questionContainer" class="col-6" style="background-color: #E3E9BB; padding-top: 18px;">
           <h1>{{ question.text }}</h1>
-
+          <img class="img-thumbnail" :src="question.img">
             <div v-for="response in question.responses" :key="response">
                 <div v-if="response.correct">
-                <h3>Correct answer: {{ response.text }}</h3>
+                <h3 style="color: #B22222;">{{ response.text }}</h3>
                 </div>
             </div>
-            {{ question.explanation }}
-        </div>
-      
+            <p>{{ question.explanation }}</p>
         </div>
     </div>
 </div>
@@ -23,6 +18,8 @@
 </template>
 
 <script>
+import quizQuestions from './QuizQuestions.vue'
+
 var quiz = {
   questions: [
     {
@@ -31,6 +28,7 @@ var quiz = {
         {text: 'Recycle it!'}, 
         {text: 'Dispose as general waste', correct: true}, 
       ],
+      img: "https://b3h2.scene7.com/is/image/BedBathandBeyond/148463261864696p?$imagePLP$&wid=256&hei=256",
       explanation: "The type of glass used in mirrors makes them hard to recycle. Dispose your mirror as general waste or donate it if you think it can be reused!"
     }, {
       text: "Unused Tissue Paper",
@@ -38,49 +36,56 @@ var quiz = {
         {text: 'Recycle it!'}, 
         {text: 'Dispose as general waste', correct: true}, 
       ],
-      explanation: "It's not often known, but you can't recycle tissue paper, even if it is unused. As a lot of tissue paper is made from already recycled paper, the fibres are too short and this will result in poor pulp in the recycling process."
+      img: "https://5.imimg.com/data5/EE/AP/GC/SELLER-92910768/toilet-paper-roll-500x500.jpg",
+      explanation: "You should not recycle tissue paper, even if it is unused. As a lot of tissue paper is made from already recycled paper, its fibres are too short to be effectively recycled."
     }, {
       text: "Milk Carton",
       responses: [
         {text: 'Recycle it!', correct: true}, 
         {text: 'Dispose as general waste'}, 
       ],
-      explanation: "Beverage cartons are recyclable! Remember to empty, rinse and flatten them before placing them into the recycling bins." 
+      img: "https://media.istockphoto.com/vectors/realistic-3d-milk-carton-packing-isolated-on-white-vector-id1028104580?k=6&m=1028104580&s=612x612&w=0&h=wLuTpnIefbO_pQIfPFcbyx4mKbRjnvQ_jMZ5FrCVLKY=",
+      explanation: "Beverage cartons are recyclable. Remember to empty, rinse and flatten them before placing them into the recycling bins." 
     }, {
       text: "Mobile Phone",
       responses: [
         {text: 'Recycle it!', correct: true}, 
         {text: 'Dispose as general waste'}, 
       ],
-      explanation: "Mobile phones, along with other Electronic waste (eg. used laptops, tablets) and IT accessories (eg. mouse, keyboard, cable) are recyclable! You can drop them off at their specific collection points for E-waste recycling. Find these E-waste bins using Recyclopedia’s Map feature!"
+      img: "https://assets.swappie.com/iPhone-11-Pro-midnight-green-back.png",
+      explanation: "Electronic waste and IT accessories are recyclable at their specific collection points for E-waste recycling. Locate these E-waste bins with Recyclopedia’s Map feature!"
     }, {
       text: "Baby Crib",
       responses: [
         {text: 'Recycle it!'}, 
         {text: 'Dispose as general waste', correct: true}, 
       ],
-      explanation: "Furniture like baby cribs should be disposed of as general waste, but remember to contact your Town council to remove from your residential premises. Alternatively, consider donating them if you think they can be reused! "
+      img: "https://i5.walmartimages.com/asr/3e5940e3-e444-4806-9ff1-46e40182b777_1.6fd989a4179b6c362d36a4a085f2c34c.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff",
+      explanation: "Furniture should be disposed of as general waste, but do contact your Town council to remove them from your residential premises. Alternatively, consider donating them if they can be reused."
     }, {
       text: "Plastic Packaging for Sliced Bread",
       responses: [
         {text: 'Recycle it!', correct: true}, 
         {text: 'Dispose as general waste'}, 
       ],
-      explanation: "Plastic packaging for food (e.g. sliced bread bag and egg trays) can be recycled as long as they are not contaminated with food waste. Remember to empty any contents before you recycle them."
+      img: "https://cdn11.bigcommerce.com/s-knrdocushe/products/8684/images/15964/8888247118113_1__14846.1526292121.500.750.jpg?c=2",
+      explanation: "Plastic packaging for food can be recycled as long as they are not contaminated with food waste. Remember to empty any contents before you recycle them."
     }, {
       text: "Plastic Drinking Straw",
       responses: [
         {text: 'Recycle it!'}, 
         {text: 'Dispose as general waste', correct: true}, 
       ],
-      explanation: "Plastic straws are already made of a low-grade plastic that deteriorates quickly, making them extremely hard to recycle as the new product will be of a value so inferior that it would just end up in the landfill." 
+      img: "https://media.springernature.com/w300/springer-static/image/art%3A10.1038%2Fbdjteam.2018.115/MediaObjects/41407_2018_Article_BFbdjteam2018115_Figa_HTML.jpg",
+      explanation: "Plastic straws are already made of a low-grade plastic that deteriorates quickly, making them extremely hard to recycle." 
     }, {
       text: "Aerosol Can",
       responses: [
         {text: 'Recycle it!', correct: true}, 
         {text: 'Dispose as general waste'}, 
       ],
-      explanation: "Ensure that aerosol cans are completely empty before recycling, but never attempt to pierce, crush or flatten them! Detach only loose or easily removable parts, such as the plastic cap, and recycle them separately. Make sure not to try removing the spray nozzle on top of the can as that can be dangerous."
+      img: "https://www.pennwaste.com/wp-content/uploads/aerosol.jpg",
+      explanation: "Ensure the cans are empty before recycling, but never attempt to pierce, crush or flatten them! Recycle loose or easily removable parts, like plastic caps, separately."
     }
   ]
 };
@@ -118,6 +123,15 @@ div {
 
 h2 {
     color: #0A4C0A;
+}
+
+p {
+    font-size: 20px;
+}
+
+.img-thumbnail {
+  height: 200px;
+  width: 200px;
 }
 
 </style>
