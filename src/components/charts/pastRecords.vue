e3dec3af5ccadf8172a00c6da1e26c38a3c15859
Choong Yong Xuan <67458324+yxchoong@users.noreply.github.com>
2021-04-14 01:19:48 +0800

    user dashboard graph

---
+++ src/components/charts/pastRecords.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chart">
+    <h5 id="center">Total Recycled for Each Category</h5>
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import Chart from "./pastRecords.js";
+export default {
+  components: {
+    Chart
+  }
+};
+</script>
+
+<style>
+#center { 
+    display: flex;
+    justify-content: center;
+    align-items:center
+}
+</style>
+
